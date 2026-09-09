57bdb48e8e3db2413426091aa0a18b697d7c79e1
Konrad Guzek <konrad@guzek.uk>
2026-09-09 20:47:01 +0200

    ci: add build workflows (#1)
    Reviewed-on: https://git.guzek.uk/kguzek/runner-images/pulls/1

---
+++ scripts/build.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+
+set -e
+
+: "${REGISTRY_HOSTNAME:?}" \
+  "${PROJECT_NAME:?}"      \
+  "${GIT_HOSTNAME:?}"      \
+  "${GIT_OWNER:?}"         \
+  "${CONTAINER_BACKEND:=docker}"
+
+export REGISTRY_HOSTNAME PROJECT_NAME
+FIRST_ARGUMENT="$1"
+
+should_push() {
+  [ "$FIRST_ARGUMENT" = '--push' ]
+}
+
+for CONTAINERFILE_PATH in ./images/*/Containerfile; do
+  REPOSITORY_PATH="${CONTAINERFILE_PATH%/Containerfile}"
+  export REPOSITORY_NAME="${REPOSITORY_PATH#./images/}"
+  eval "$(./scripts/metadata.sh)"
+
+  TAG_REGISTRY="$REGISTRY_HOSTNAME/$PROJECT_NAME/$REPOSITORY_NAME"
+  TAG_GIT="$GIT_HOSTNAME/$GIT_OWNER/$PROJECT_NAME/$REPOSITORY_NAME"
+
+  TAG_REGISTRY_LATEST="$TAG_REGISTRY:latest"
+  TAG_REGISTRY_VERSIONED="$TAG_REGISTRY:$IMAGE_VERSION"
+  TAG_GIT_LATEST="$TAG_GIT:latest"
+  TAG_GIT_VERSIONED="$TAG_GIT:$IMAGE_VERSION"
+
+  "$CONTAINER_BACKEND" build "$REPOSITORY_PATH"        \
+    --file "$CONTAINERFILE_PATH"                       \
+    --build-arg "REGISTRY_HOSTNAME=$REGISTRY_HOSTNAME" \
+    --build-arg "PROJECT_NAME=$PROJECT_NAME"           \
+    --build-arg "REPOSITORY_NAME=$REPOSITORY_NAME"     \
+    --build-arg "GIT_HOSTNAME=$GIT_HOSTNAME"           \
+    --build-arg "GIT_OWNER=$GIT_OWNER"                 \
+    --build-arg "IMAGE_CREATED=$IMAGE_CREATED"         \
+    --build-arg "IMAGE_VERSION=$IMAGE_VERSION"         \
+    --build-arg "IMAGE_REVISION=$IMAGE_REVISION"       \
+    --tag "$TAG_REGISTRY_LATEST"                       \
+    --tag "$TAG_REGISTRY_VERSIONED"                    \
+    --tag "$TAG_GIT_LATEST"                            \
+    --tag "$TAG_GIT_VERSIONED"
+
+  if ! should_push; then
+    continue
+  fi
+
+  for TAG in "$TAG_REGISTRY_LATEST" "$TAG_REGISTRY_VERSIONED" "$TAG_GIT_LATEST" "$TAG_GIT_VERSIONED"; do
+    "$CONTAINER_BACKEND" push "$TAG"
+  done
+  cosign sign --new-bundle-format=false --use-signing-config=false "$TAG_REPOSITORY_LATEST"
+done
---
+++ scripts/metadata.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+: "${REGISTRY_HOSTNAME:?}" \
+  "${PROJECT_NAME:?}"      \
+  "${REPOSITORY_NAME:?}"
+
+IMAGE_CREATED=$(date -u +'%Y-%m-%dT%H:%M:%SZ')
+IMAGE_VERSION=$(date -u +'v%y.%m.%d')
+IMAGE_REVISION=$(git rev-parse HEAD)
+
+LATEST_MATCHING_VERSION=$(
+  skopeo list-tags "docker://$REGISTRY_HOSTNAME/$PROJECT_NAME/$REPOSITORY_NAME" | \
+  jq -r ".Tags[] | select(startswith(\"$IMAGE_VERSION\"))"                     | \
+  sort --numeric-sort --reverse                                                 | \
+  head --lines 1
+)
+
+if [ -n "$LATEST_MATCHING_VERSION" ]; then
+  SUFFIX="${LATEST_MATCHING_VERSION#"$IMAGE_VERSION"}"
+  if [ -z "$SUFFIX" ]; then
+    PREVIOUS_REVISION="0"
+  else
+    PREVIOUS_REVISION="${SUFFIX#-}"
+  fi
+  IMAGE_VERSION="$IMAGE_VERSION-$(( PREVIOUS_REVISION + 1 ))"
+fi
+
+echo "IMAGE_CREATED=$IMAGE_CREATED"
+echo "IMAGE_VERSION=$IMAGE_VERSION"
+echo "IMAGE_REVISION=$IMAGE_REVISION"
